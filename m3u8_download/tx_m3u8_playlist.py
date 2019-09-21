5ad85551d7974ec5f0f79ed3470e61b42ba92535
xuruidong <xuruidong@gmail.com>
2019-09-22 01:19:00 +0800

    nothing

--- m3u8_download/tx_m3u8_playlist.py
+++ m3u8_download/tx_m3u8_playlist.py
@@ -29,7 +29,7 @@ class choose_m3u8(object):
 
 if __name__ == "__main__":
     
-    url = "https://1258712167.vod2.myqcloud.com/fb8e6c92vodtranscq1258712167/1db02b835285890793310354501/drm/voddrm.token.dWluPTk4Njg3MjU4Mzt2b2RfdHlwZT0wO2NpZD0yNDQ5Mzg7dGVybV9pZD0xMDA1MDg1NjE7cGxza2V5PTAwMDQwMDAwZTdiMDM1ZmQ0NDAwMjViODUwYTY0ZjliYTQ4NDhhYzM2MDE5OTE4ZWQxMjJmNWMwMWE4YmY2ZTQ3ZDBlOWU3ZWM0MWQ4OTUzNjc2NmZkNjI7cHNrZXk9.master_playlist.m3u8?t=5d8c19d0&exper=0&us=9187067277007065424&sign=cb54951d20f40abec571fa36b9e11d1d"
+    url = "https://"
     playlist = choose_m3u8(url)
     playlist.m3u8_parse()
     print ("end")
