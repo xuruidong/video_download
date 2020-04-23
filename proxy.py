b2071b0ddd048de9d12902f888a23481ab838b6e
xuruidong <xuruidong@gmail.com>
2020-04-23 17:32:52 +0800

    filename

--- proxy.py
+++ proxy.py
@@ -61,7 +61,8 @@ def thread_fun():
 
             if(end[-1] in ["mp4","flv", "f4v", "m3u8"]):
                 
-                filename = tmp_list[0].split("/")[-1]
+                #filename = tmp_list[0].split("/")[-1]
+                filename = tmp_list[0]
                 if (filename in old_file_list):
                     continue
                     #pass
